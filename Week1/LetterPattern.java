797fbdf763d0b42af665f5439d503d22b548a69f
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-06-23 16:46:10 +0600

    Create LetterPattern.java

---
+++ Week1/LetterPattern.java
@@ -0,0 +1,14 @@
+/*
+Write a code that prints out this pattern with letters.(pic)
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("    A    \n   BAB   \n  CBABC  \n DCBABCD \nEDCBABCDE");
+    }
+}
