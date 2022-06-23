99e5a9abff54fe881766512ae94c54790f02be7b
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-06-23 16:48:48 +0600

    Create Challenge2.java

---
+++ Week2/Challenge2.java
@@ -0,0 +1,19 @@
+/*
+Write a program in which you enter 2 integers a and b, swap the values of the variables.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        
+        int a = in.nextInt();
+        int b = in.nextInt();
+            System.out.println("a = "+b);
+            System.out.println("b = "+a);
+    }
+}
