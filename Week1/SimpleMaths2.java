b9595b93bd103e45c9ff64730efb7efda39dfe64
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-06-23 16:45:21 +0600

    Create SimpleMaths2.java

---
+++ Week1/SimpleMaths2.java
@@ -0,0 +1,16 @@
+/*
+
+(Summation of a series) Write a program that displays the result of: (pic)
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println(1+2+3+4+5+6+7+8+9);
+    }
+}
