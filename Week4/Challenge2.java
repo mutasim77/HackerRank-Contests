2b65085132fa1e77c26946166af544e7a11958d5
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-06-23 17:00:47 +0600

    Create Challenge2.java

---
+++ Week4/Challenge2.java
@@ -0,0 +1,15 @@
+/*
+Write a program that takes text and prints the length (number of characters)
+of the text to the screen.
+*/
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s= in.next();//University
+        int a = s.length(); //University == 10
+        System.out.println(a);
+    }
+}
