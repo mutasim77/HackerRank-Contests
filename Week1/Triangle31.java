c9fe23a605d93a7af04d6cd0ec70dfe7f2366e78
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-06-23 16:45:46 +0600

    Create Triangle31.java

---
+++ Week1/Triangle31.java
@@ -0,0 +1,14 @@
+/*
+Try to write a code that prints out a triangle pattern with stars. Do not forget that spaces are also sign.(pic)
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("   *   \n  ***  \n *****\n*******");
+    }
+}
