1eccf57b816f08bb13194e8a4b0a32c727f4ff13
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-06-23 16:44:25 +0600

    Create info1.java

---
+++ Week1/info1.java
@@ -0,0 +1,24 @@
+/*
+Hey there, you are welcome to the world of Java! In this challenge, we will practice printing to stdout.
+The code stubs in your editor declare a Solution class and a main method.
+Complete the main method by writing a code that prints out this 3 lines of text
+*/
+
+/*
+
+Output Format:
+Hello for all!
+I'm a student of SDU!!!
+I love JAVA!!!
+
+*/
+
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+      
+        System.out.println("Hello for all!\nI'm a student of SDU!!!\nI love JAVA!!!");
+    
+    }
+}
