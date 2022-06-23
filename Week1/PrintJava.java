02f16f49eb3cd6bd29f3eda8e9c5b31ad430498a
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-06-23 16:46:32 +0600

    Create PrintJava.java

---
+++ Week1/PrintJava.java
@@ -0,0 +1,20 @@
+/*
+Write a program that displays the following pattern:(pic.)
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        String s = 
+"    J     A     V     V     A\n"+
+"    J    A A     V   V     A A\n"+
+"J   J   AAAAA     V V     AAAAA\n"+
+" J J   A     A     V     A     A\n";
+        
+      System.out.println(s);
+    }
+}
