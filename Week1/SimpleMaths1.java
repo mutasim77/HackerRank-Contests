352c4a5104c009638cf88e43ffb7feab655c53c4
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-06-23 16:44:53 +0600

    Create SimpleMaths1.java

---
+++ Week1/SimpleMaths1.java
@@ -0,0 +1,14 @@
+/*
+
+(Compute expressions) Write a program that displays the result of:(pic)
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println((9.5*4.5 - 2.5*3) / (45.5 - 3.5));
+    }
+}
